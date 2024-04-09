14fb1e426653a5d26e726c51ae17b1c15f73ef91
195785 <124942053+195785@users.noreply.github.com>
2024-04-08 21:35:41 -0300

    Add files via upload

---
+++ Delete Usuarios.sql
@@ -0,0 +1,15 @@
+DELETE FROM Usuarios
+WHERE Nome='João Silva' AND CPF = '123.456.789-00' AND Telefone = '(11)9999-8888'
+
+SELECT * FROM Usuarios
+
+
+
+INSERT into usuarios (Nome,CPF,Telefone)
+VALUES ('Carlos Alberto', 12345678990, 11999899076),
+       ('Solange Gomes', 45798900090, 655554444),
+	   ('Gustavo Henrique', 23478965499,  13779899087);
+
+SELECT * FROM Usuarios
+
+       
